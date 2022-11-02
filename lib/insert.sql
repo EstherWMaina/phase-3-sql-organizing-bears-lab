687c929da6245ff1b36ccf50130a70fb0b48204c
Esther Maina <mainaesther77@gmial.com>
2022-11-02 22:33:54 +0300

    Completed assugnment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,'Mr. Chocolate', 3, 'F', 'White', 'Normal', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 2, 'M', 'Black', 'Normal', '1');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 1, 'F', 'Brown', 'High', '1');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 3, 'M', 'White', 'Normal', '0');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 2, 'F', 'Grey', 'Low', '1');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 4, 'M', 'Black', 'Normal', '0');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 2, 'F', 'Brown', 'High', '1');
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 1, 'M', 'Grey', 'Normal', '0');
